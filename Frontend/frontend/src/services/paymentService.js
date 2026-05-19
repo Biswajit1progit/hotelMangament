7f1e2f5b348a49ec1070f7eb8909745af154040f
Biswajit Sahoo <biswajitsahookalia@gmail.com>
2026-05-19 09:10:23 +0530

    Fixed production API URL

--- Frontend/frontend/src/services/paymentService.js
+++ Frontend/frontend/src/services/paymentService.js
@@ -1,5 +1,5 @@
  export const createOrder = async (amount) => {
-  const res = await fetch("http://localhost:5000/api/payment/create-order", {
+  const res = await fetch(`${process.env.REACT_APP_API_URL}/api/payment/create-order`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ amount }),
