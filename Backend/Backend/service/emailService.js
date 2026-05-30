2815f33c2bf5c40023fc374fac43a9c6e5c04f81
Biswajit Sahoo <biswajitsahookalia@gmail.com>
2026-05-31 00:39:56 +0530

    new change on email

--- Backend/Backend/service/emailService.js
+++ Backend/Backend/service/emailService.js
@@ -8,8 +8,18 @@
 
 const nodemailer = require("nodemailer")
 
-const transporter = nodemailer.createTransport({
+/* const transporter = nodemailer.createTransport({
   service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+}) */
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,          // ← forces IPv4, fixes Render's IPv6 issue
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
