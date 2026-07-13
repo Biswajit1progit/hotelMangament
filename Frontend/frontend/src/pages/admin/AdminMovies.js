69df020ec386b17896132e527344ffb0e71c7deb
Biswajit Sahoo <biswajitsahookalia@gmail.com>
2026-07-13 10:08:58 +0530

    new

--- Frontend/frontend/src/pages/admin/AdminMovies.js
+++ Frontend/frontend/src/pages/admin/AdminMovies.js
@@ -11,7 +11,7 @@ import {
   createShow,
   deleteShow,
   getShowsForMovie,
-} from "../../services/movieService";
+} from "../../services/Movieservice";
 
 const SECTIONS = [
   { key: "movies", label: "🎬 Movies" },
